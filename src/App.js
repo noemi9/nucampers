aa7b11a8ce8612130a8a64bb4e02a84324e0e31b
Noemi_B <msnbernal@gmail.com>
2024-04-06 12:27:07 -0600

    The App Component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ import NucampLogo from './app/assets/img/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      hello nucamp!
+    <div className='App'>
+      <Navbar dark color='primary' sticky='top' expand='md'>
+               <Container>
+                    <NavbarBrand href='/'>
+                        <img src={NucampLogo} alt='nucamp logo' />
+                    </NavbarBrand>
+                </Container>
+            </Navbar>
+      I'm ready for workshop!
     </div>
   );
 }
