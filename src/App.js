33211919723df1a269b41dc86cf64cc22ded786c
Noemi_B <msnbernal@gmail.com>
2024-04-06 19:16:30 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React from 'react';
+
 import './App.css';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+import Header from './components/Header.js';
 import CampsitesList from './features/campsites/CampsitesList.js';
-
-
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className='App'>
-      <Navbar dark color='primary' sticky='top' expand='md'>
-               <Container>
-                    <NavbarBrand href='/'>
-                        <img src={NucampLogo} alt='nucamp logo' />
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
-            < CampsitesList />
+      <Header/>
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,20 @@
-import {Navbar, NavbarBrand} from reactstrap
+import {Navbar, NavbarBrand} from 'reactstrap';
+import NucampLogo from '../app/assets/img/logo.png';
+
+const Header = () => {
+    return (
+        <Navbar dark color='primary' sticky='top' expand='md'>
+                    <NavbarBrand href='/'>
+                        <img src={NucampLogo} alt='nucamp logo' />
+                    </NavbarBrand>
+            </Navbar>
+    )
+}
+
+export default Header;
+
 
 //can yall see this????
 
 //my power just cut out so im using my hotspot on my phone. 
+
